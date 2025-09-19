7d600a6b0622a389ba40077017c2dd97b7e6ce0a
Sibas <40043677+SiBas@users.noreply.github.com>
2025-09-19 12:26:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const playBtn = document.querySelector(".play-btn");
+  playBtn.addEventListener("click", () => {
+    alert("Video Play Simulation - Integrasi YouTube/Embed bisa ditambahkan.");
+  });
+});
