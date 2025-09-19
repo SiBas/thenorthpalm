ac022048181afd0b517c0c79a6453dc9588d87b9
Sibas <40043677+SiBas@users.noreply.github.com>
2025-09-19 16:05:26 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const playBtn = document.querySelector(".play-btn");
-  playBtn.addEventListener("click", () => {
-    alert("Video Play Simulation - Integrasi YouTube/Embed bisa ditambahkan.");
-  });
+// Featured Video Slider New
+const wrapper = document.querySelector('.video-wrapper');
+const slides = document.querySelectorAll('.video-slide');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let index = 0;
+
+function showSlide(i) {
+  if (i >= slides.length) index = 0;
+  if (i < 0) index = slides.length - 1;
+  wrapper.style.transform = `translateX(${-index * 100}%)`;
+}
+
+nextBtn.addEventListener('click', () => {
+  index++;
+  showSlide(index);
+});
+
+prevBtn.addEventListener('click', () => {
+  index--;
+  showSlide(index);
 });
